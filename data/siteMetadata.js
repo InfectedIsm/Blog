9890c3535601dcdf3402e47e349a348b6df93dfe
InfectedIsm <52218361+InfectedIsm@users.noreply.github.com>
2024-07-09 17:56:11 +0200

    update

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Another Infected Blog',
   author: 'InfectedCrypto',
   headerTitle: 'Another Infected Blog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'A blog about web3 security',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
