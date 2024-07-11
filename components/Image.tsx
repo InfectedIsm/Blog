ab6332c285a8cd1a03052fb55e38f11c1cf70211
InfectedIsm <52218361+InfectedIsm@users.noreply.github.com>
2024-07-11 11:41:18 +0200

    update: fixing vercel build failing (1)

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,24 +1,49 @@
 import React from 'react';
 import 'css/image.css'; // Ensure you have a CSS file to add the necessary styles
 
-const Image = ({ src, alt, ...rest }) => {
+import NextImage, { ImageProps } from 'next/image'
+const basePath = process.env.BASE_PATH
+
+const Image = ({ src, alt, ...rest }: ImageProps) => {
 	return (
 	  <div>
 		<label htmlFor={`lightbox-${src}`} className="cursor-pointer block">
-		  <img src={src} alt={alt} {...rest} />
+		  <NextImage src={`${basePath || ''}${src}`} alt={alt} {...rest} />
 		</label>
 		<input type="checkbox" id={`lightbox-${src}`} className="lightbox-toggle" />
 		<div className="lightbox">
 		  <label htmlFor={`lightbox-${src}`} className="lightbox-close">&times;</label>
 		  <div className="lightbox-content">
-			<img src={src} alt={alt} className="max-w-full max-h-full" />
+			<img src={`${basePath || ''}${src}`} alt={alt} className="max-w-full max-h-full" />
 		  </div>
 		</div>
 	  </div>
-	);
-  };
+	)
+  }
   
-  export default Image;
+  export default Image
+
+// import React from 'react';
+// import 'css/image.css'; // Ensure you have a CSS file to add the necessary styles
+
+// const Image = ({ src, alt, ...rest }) => {
+// 	return (
+// 	  <div>
+// 		<label htmlFor={`lightbox-${src}`} className="cursor-pointer block">
+// 		  <img src={src} alt={alt} {...rest} />
+// 		</label>
+// 		<input type="checkbox" id={`lightbox-${src}`} className="lightbox-toggle" />
+// 		<div className="lightbox">
+// 		  <label htmlFor={`lightbox-${src}`} className="lightbox-close">&times;</label>
+// 		  <div className="lightbox-content">
+// 			<img src={src} alt={alt} className="max-w-full max-h-full" />
+// 		  </div>
+// 		</div>
+// 	  </div>
+// 	);
+//   };
+  
+//   export default Image;
 
 // import React from 'react';
 // import 'css/image.css'; // Ensure you have a CSS file to add the necessary styles
@@ -27,14 +52,14 @@ const Image = ({ src, alt, ...rest }) => {
 //   return (
 //     <div className="image-wrapper">
 //       <label htmlFor={`lightbox-${src}`} className="cursor-pointer block">
-//         <img src={src} alt={alt} width={width} height={height} {...rest} />
+//         <NextImage src={src} alt={alt} width={width} height={height} {...rest} />
 //       </label>
 //       <input type="checkbox" id={`lightbox-${src}`} className="lightbox-toggle" />
 //       <div className="lightbox">
 //         <label htmlFor={`lightbox-${src}`} className="lightbox-background"></label>
 //         <div className="lightbox-content">
 //           <label htmlFor={`lightbox-${src}`} className="lightbox-close">&times;</label>
-//           <img src={src} alt={alt} className="max-w-full max-h-full" />
+//           <NextImage src={src} alt={alt} className="max-w-full max-h-full" />
 //         </div>
 //       </div>
 //     </div>
