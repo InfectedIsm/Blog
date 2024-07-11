067fe3b549d9385e908f1bd2cd649e77d913be2b
InfectedIsm <52218361+InfectedIsm@users.noreply.github.com>
2024-07-11 11:21:33 +0200

    update: image.tsx comment: remove "width, height" property as vercel build fails

--- components/Image.tsx
+++ components/Image.tsx
@@ -1,44 +1,44 @@
-// import React from 'react';
-// import 'css/image.css'; // Ensure you have a CSS file to add the necessary styles
-
-// const Image = ({ src, alt, ...rest }) => {
-// 	return (
-// 	  <div>
-// 		<label htmlFor={`lightbox-${src}`} className="cursor-pointer block">
-// 		  <img src={src} alt={alt} {...rest} />
-// 		</label>
-// 		<input type="checkbox" id={`lightbox-${src}`} className="lightbox-toggle" />
-// 		<div className="lightbox">
-// 		  <label htmlFor={`lightbox-${src}`} className="lightbox-close">&times;</label>
-// 		  <div className="lightbox-content">
-// 			<img src={src} alt={alt} className="max-w-full max-h-full" />
-// 		  </div>
-// 		</div>
-// 	  </div>
-// 	);
-//   };
-  
-//   export default Image;
-
 import React from 'react';
 import 'css/image.css'; // Ensure you have a CSS file to add the necessary styles
 
-const Image = ({ src, alt, width, height, ...rest }) => {
-  return (
-    <div className="image-wrapper">
-      <label htmlFor={`lightbox-${src}`} className="cursor-pointer block">
-        <img src={src} alt={alt} width={width} height={height} {...rest} />
-      </label>
-      <input type="checkbox" id={`lightbox-${src}`} className="lightbox-toggle" />
-      <div className="lightbox">
-        <label htmlFor={`lightbox-${src}`} className="lightbox-background"></label>
-        <div className="lightbox-content">
-          <label htmlFor={`lightbox-${src}`} className="lightbox-close">&times;</label>
-          <img src={src} alt={alt} className="max-w-full max-h-full" />
-        </div>
-      </div>
-    </div>
-  );
-};
+const Image = ({ src, alt, ...rest }) => {
+	return (
+	  <div>
+		<label htmlFor={`lightbox-${src}`} className="cursor-pointer block">
+		  <img src={src} alt={alt} {...rest} />
+		</label>
+		<input type="checkbox" id={`lightbox-${src}`} className="lightbox-toggle" />
+		<div className="lightbox">
+		  <label htmlFor={`lightbox-${src}`} className="lightbox-close">&times;</label>
+		  <div className="lightbox-content">
+			<img src={src} alt={alt} className="max-w-full max-h-full" />
+		  </div>
+		</div>
+	  </div>
+	);
+  };
+  
+  export default Image;
 
-export default Image;
+// import React from 'react';
+// import 'css/image.css'; // Ensure you have a CSS file to add the necessary styles
+
+// const Image = ({ src, alt, width, height, ...rest }) => {
+//   return (
+//     <div className="image-wrapper">
+//       <label htmlFor={`lightbox-${src}`} className="cursor-pointer block">
+//         <img src={src} alt={alt} width={width} height={height} {...rest} />
+//       </label>
+//       <input type="checkbox" id={`lightbox-${src}`} className="lightbox-toggle" />
+//       <div className="lightbox">
+//         <label htmlFor={`lightbox-${src}`} className="lightbox-background"></label>
+//         <div className="lightbox-content">
+//           <label htmlFor={`lightbox-${src}`} className="lightbox-close">&times;</label>
+//           <img src={src} alt={alt} className="max-w-full max-h-full" />
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Image;
