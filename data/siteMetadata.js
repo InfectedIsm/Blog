f7e4acabd85d1dff3eb234df393631bbfad8e31b
InfectedIsm <52218361+InfectedIsm@users.noreply.github.com>
2024-12-27 22:13:12 +0100

    feat: adding giscus comment manager

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -60,17 +60,17 @@ const siteMetadata = {
     giscusConfig: {
       // Visit the link below, and follow the steps in the 'configuration' section
       // https://giscus.app/
-      repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
-      repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
-      category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
-      categoryId: process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
-      mapping: 'pathname', // supported options: pathname, url, title
+      repo: 'InfectedIsm/Blog',
+      repositoryId: 'R_kgDOLv9-VA',
+      category: 'Announcements',
+      categoryId: 'DIC_kwDOLv9-VM4Cljz-',
+      mapping: 'title', // supported options: pathname, url, title
       reactions: '1', // Emoji reactions: 1 = enable / 0 = disable
       // Send discussion metadata periodically to the parent window: 1 = enable / 0 = disable
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      theme: 'light',
+      theme: 'preferred_color_scheme',
       // theme when dark mode
       darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
